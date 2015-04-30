41ff57663335276e925c2e49819d6ca695678690
Fernando Copa <copa.fernando@gmail.com>
2015-04-30 15:04:59 -0300

    Implementado la funcion doc_get_word. Si no funciona bien esta funcion, hechar un ojo primero a la funcion palabra.

--- lab3/spellchecker/src/Document.hs
+++ lab3/spellchecker/src/Document.hs
@@ -46,6 +46,53 @@ doc_get_word (Document docIn docOut) =
     let
     -- hgetchar
     -- hseek
+        {-------------------------------------------------------------
+            fun lee una palabra.
+            input: Entrada, Salida (de tipo Handle)
+            return: String
+           
+            Nota: Cuando lee el ultimo char lo manda a salida.
+            Es decir, al leer "Hola como estas", lee "hola" y
+            el puntero queda en el ' ' lo cual lo llevo a salida.
+        -------------------------------------------------------------}
+        palabra h sal = do 
+            s <- hIsEOF h
+            if s then return [] else do 
+                c <- hGetChar h
+                if isAlpha c then do
+                    r <- palabra h sal
+                    return (c:r)
+                    else do
+                            hPutChar sal c
+                            return []
+
+        {-------------------------------------------------------------
+            fun lee un String
+            Input: Entrada, Salida (de tipo Handle)
+            return: String
+
+            Nota: A diferencia de fun palabra, este le el string del
+            estilo " hola", llevando el ' ' a la salida. Usa palabra
+            para obtener "hola".
+        -------------------------------------------------------------}
+        leerConRaro h sal = do
+            e <- hIsEOF h
+            if e then return [] else do
+                c <- hGetChar h
+                if not (isAlpha c) then do -- Es char raro
+                    pal <- palabra h sal -- Leo lo que viene despues del raro
+                    hPutChar sal c
+                    return pal 
+                    else do
+                        s <- palabra h sal
+                        return (c:s)
+
+    in
+        do
+            xs <- leerConRaro a b
+            -- hClose a
+            -- hClose b
+            return xs
 
 -- Escribe una palabra en el documento de salida.
 doc_put_word :: Word -> Document -> IO ()
