9beb44c0919cecf9fc4ab55e7fbfe29331508725
Fernando Copa <copa.fernando@gmail.com>
2015-06-10 22:48:41 -0300

    Avanzando con algunas funciones. Solo falta el procesar documento

--- lab6/spellchecker/spellchecker.sh
+++ lab6/spellchecker/spellchecker.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
 # Parameter to initialize awk script.
 FILE_DICT_MAIN=$1
 FILE_DOC_IN=$2
-FILE_DICT_OUT="out.txt"
+FILE_DICT_OUT="$1"
+FILE_OUT="out.txt"
 
 E_PARAMERR=85
 dict_main=()
 dict_ignored=()
+file_out=()
 current_word=""
 word_is_known=false
+
+first_chars=""
+last_chars=""
 
 dict_save() {
     printf "%s\n" "${dict_main[@]}" > "${FILE_DICT_OUT}"
@@ -41,15 +47,43 @@ is_known() {
 }
 
 get_word() {
-    :
+    word=""
+    for c in "$current_word"
+    do
+        if [[ $c = [a-zA-Z] ]]
+        then
+            word=$word$s
+        else
+            file_out+=("$c")
+        fi
+    done
+    current_word=$word
 }
 
 put_word() {
-    :
+    file_out+=$current_word
 }
 
 consult_user() {
-    :
+    echo -n "Aceptar(a) - Reemplazar(r) - Ignorar(i): "
+    read op
+    case $op in
+        [aA] )
+            dict_add
+            ;;
+        [rR] )
+            echo -n "Reemplazar por: "
+            read res
+            current_word=$res
+            ;;
+        [iI] )
+            dict_ignored+=("$current_word")
+            ;;
+        *)
+            echo "Invalid input"
+            current_word
+            ;;
+    esac
 }
 
 process_doc() {
@@ -68,15 +102,20 @@ then
     usage   # Print usage message and exit.
 fi
 
-dict_load
-current_word="hola"
-is_known
-if [ "$word_is_known" = false ]
-then
-    dict_add
-    dict_save
-fi
+# dict_load
+# current_word="hola"
+# is_known
+# if [ "$word_is_known" = false ]
+# then
+#     dict_add
+#     dict_save
+# fi
+VALOR='G'
 
+if [[ $VALOR = [a-zA-Z] ]]
+then
+    echo "$VALOR"
+fi
 exit $?
 
 # Por las dudas
