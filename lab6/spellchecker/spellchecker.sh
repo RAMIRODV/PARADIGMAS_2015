4d53787437b4805208897239548789d4459649a1
RAMIRODV <della.wrc@gmail.com>
2015-06-13 09:08:45 -0300

    Andando. Falta terminar de testear y agregar detalles...

--- lab6/spellchecker/spellchecker.sh
+++ lab6/spellchecker/spellchecker.sh
@@ -1,125 +1,88 @@
 #!/bin/bash
 
-# Parameter to initialize awk script.
-FILE_DICT_MAIN=$1
-FILE_DOC_IN=$2
-FILE_DICT_OUT="$1"
+FILE_DOC_IN=$1
+FILE_DICT_MAIN=$2
+FILE_DICT_OUT=""
 FILE_OUT="out.txt"
+dict_ignored=""
+word=""
+file_out=""
 
-E_PARAMERR=85
-dict_main=()
-dict_ignored=()
-file_out=()
-current_word=""
-word_is_known=false
-
-first_chars=""
-last_chars=""
-
-dict_save() {
-    printf "%s\n" "${dict_main[@]}" > "${FILE_DICT_OUT}"
-}
-
-dict_load() {
-    i=0
-    while read line
+process_document() {
+    flag=0
+    while IFS= read -r -N 1 -u9 character
     do
-        for word in $line; do
-            dict_main[i]=$word
-            i=$(($i + 1))
-        done
-    done < "${FILE_DICT_MAIN}"
-}
-
-dict_add() {
-    dict_main+=($current_word)
-}
-
-is_known() {
-    for e in "${dict_main[@]}"
-    do
-        if [ $e = $current_word ]
+        if [[ $character = [a-zA-Z] ]]
         then
-            word_is_known=true
-            break
-        fi
-    done
-}
-
-get_word() {
-    word=""
-    for c in "$current_word"
-    do
-        if [[ $c = [a-zA-Z] ]]
-        then
-            word=$word$s
+            word+=$character
+            flag=1
         else
-            file_out+=("$c")
+            if (($flag == 1))
+            then
+                is_know_dict=$(grep -ci $word $FILE_DICT_OUT)
+                is_know_ignored=$(grep -ci $word $dict_ignored)
+                let is_know=is_know_dict+is_know_ignored
+                if (( $is_know == 0))
+                then
+                    echo "Palabra no reconocida: $word";
+                    echo "Aceptar (a) - Ignorar (i) - Reemplazar (r): ";
+                    while true;
+                    do
+                        read -p "Ingrese opcion: " op;
+                        if [ "$op" == 'a' ]
+                        then
+                            echo $word >> $FILE_DICT_OUT
+                            break
+                        elif [ "$op" == 'i' ]
+                        then
+                            echo $word >> $dict_ignored
+                            break
+                        elif [ "$op" == 'r' ]
+                        then
+                            read -p "Ingrese una nueva palabra: " res;
+                            word=$res
+                            break
+                        else
+                            echo "ERROR: Opción Invalida.";
+                        fi
+                    done
+                fi
+                file_out+=$word
+                word=""
+                flag=0
+            fi
+            if [[ "$character" == $'\n' ]];
+            then
+                file_out+="\n"
+            elif [[ "$character" == $' ' ]];
+            then
+                file_out+=" "
+            else
+                file_out+=$character
+            fi
         fi
-    done
-    current_word=$word
+    done 9< "${FILE_DOC_IN}"
 }
 
-put_word() {
-    file_out+=$current_word
+main() {
+    if (( $# < 1 )); then
+        echo "spellchecker.sh: nro de argumentos erroneo. Deben ser <documento> [<diccionario>]."
+        exit
+    elif (( $# == 1 )); then
+        FILE_DICT_OUT="dict.txt"
+        touch $FILE_DICT_OUT
+    else
+        FILE_DICT_OUT=$2
+    fi
+    dict_ignored="dict_ignored.txt"
+    touch $dict_ignored
+    FILE_DOC_IN=$1
+    process_document
+    touch $FILE_OUT
+    rm $FILE_OUT
+    echo -e $file_out >> $FILE_OUT
+    rm $dict_ignored
+    echo "El documento '$FILE_DOC_IN' ha sido procesado. Resultados en out.txt"
 }
 
-consult_user() {
-    echo -n "Aceptar(a) - Reemplazar(r) - Ignorar(i): "
-    read op
-    case $op in
-        [aA] )
-            dict_add
-            ;;
-        [rR] )
-            echo -n "Reemplazar por: "
-            read res
-            current_word=$res
-            ;;
-        [iI] )
-            dict_ignored+=("$current_word")
-            ;;
-        *)
-            echo "Invalid input"
-            current_word
-            ;;
-    esac
-}
-
-process_doc() {
-    :
-}
-
-usage() {
-    echo "Usage: spellchecker.sh <dict-file> <doc-file>" 2>&1
-    # For example:   ./spellchecker.sh dict.txt doc.txt
-    exit $E_PARAMERR  # Too few arguments passed to script.
-}
-
-# Verifica si se pasan bien los argumentos.
-if [ ! -f "$1" ] || [ ! -f "$2" ]
-then
-    usage   # Print usage message and exit.
-fi
-
-# dict_load
-# current_word="hola"
-# is_known
-# if [ "$word_is_known" = false ]
-# then
-#     dict_add
-#     dict_save
-# fi
-VALOR='G'
-
-if [[ $VALOR = [a-zA-Z] ]]
-then
-    echo "$VALOR"
-fi
-exit $?
-
-# Por las dudas
-#    for e in "${dict_main[@]}"
-#    do
-#        echo "$e"
-#    done
+main $FILE_DOC_IN $FILE_DICT_MAIN
